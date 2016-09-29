85683fb10ffc473b0f1d7e15848a87e0e07e619e
Valentin Samir <valentin.samir@crans.org>
2016-09-29 09:45:17 +0200

    Add tests for Category

--- src/test/groovy/org/tsaap/competencies/CompetenceServiceTest.groovy
+++ src/test/groovy/org/tsaap/competencies/CompetenceServiceTest.groovy
@@ -1,6 +1,7 @@
 package org.tsaap.competencies
 
 import org.tsaap.competencies.repositories.CatalogRepository
+import org.tsaap.competencies.repositories.CategoryRepository
 import spock.lang.Specification
 
 
@@ -12,10 +13,12 @@ class CompetenceServiceTest extends Specification {
 
     private CompetenceService competenceService
     private CatalogRepository catalogRepository
+    private CategoryRepository categoryRepository
 
     void setup() {
         catalogRepository = Mock(CatalogRepository)
-        competenceService = new CompetenceService(catalogRepository)
+        categoryRepository = Mock(CategoryRepository)
+        competenceService = new CompetenceService(catalogRepository, categoryRepository)
     }
 
     def "test save a valid catalog"() {
@@ -29,5 +32,15 @@ class CompetenceServiceTest extends Specification {
         1 * catalogRepository.save(catalog)
     }
 
+    def "test save a valid category"() {
+        given: "a category"
+        def category = Mock(Category)
+
+        when: "the category is saved"
+        competenceService.saveCategory(category)
+
+        then: "categoryRepository save is called"
+        1 * categoryRepository.save(category)
+    }
 
 }
