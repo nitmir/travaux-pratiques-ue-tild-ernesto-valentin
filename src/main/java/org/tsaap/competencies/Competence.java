1e9c1b3fd2847ea166e18a1e9243fd1983076408
Valentin Samir <valentin.samir@crans.org>
2016-10-04 08:50:28 +0200

    Remove setId on class Category and Competence

--- src/main/java/org/tsaap/competencies/Competence.java
+++ src/main/java/org/tsaap/competencies/Competence.java
@@ -33,10 +33,6 @@ public class Competence {
         return id;
     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
     public String getName() {
         return name;
     }
